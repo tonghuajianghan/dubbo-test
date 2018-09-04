69674001f26403749317257a9754c6ceab964474
jh <JIANGHAN496@paicdom.local>
2018-09-04 11:14:30 +0800

    dubbo 结构   dubbo   guava   netty   zookeeper

---
+++ zookeeper/src/main/java/zookeeper/ZooKeeperConnection.java
@@ -0,0 +1,39 @@
+package zookeeper;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by jianghan on 2018/8/31.
+ */
+public class ZooKeeperConnection {
+    // declare zookeeper instance to access ZooKeeper ensemble
+    private ZooKeeper zoo;
+    final CountDownLatch connectedSignal = new CountDownLatch(1);
+
+    // Method to connect zookeeper ensemble.
+    public ZooKeeper connect(String host) throws IOException,InterruptedException {
+
+        zoo = new ZooKeeper(host,5000,new Watcher() {
+
+            public void process(WatchedEvent we) {
+
+                if (we.getState() == Event.KeeperState.SyncConnected) {
+                    connectedSignal.countDown();
+                }
+            }
+        });
+
+        connectedSignal.await();
+        return zoo;
+    }
+
+    // Method to disconnect from zookeeper server
+    public void close() throws InterruptedException {
+        zoo.close();
+    }
+}
